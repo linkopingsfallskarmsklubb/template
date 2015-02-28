5780e1159da6d67a9cbcb7111378796d2a600210
Christian Svensson <blue@cmd.nu>
2015-02-28 21:39:21 +0000

    Social medias in the footer

--- index.php
+++ index.php
@@ -122,7 +122,22 @@ if (is_dir($localdir)) {
     <jdoc:include type="modules" name="bottom" />
   </div>
   <div id="lfk-footer">
-    <div class="fb-like" data-href="http://linkopingsfallskarmsklubb.se/" data-width="250" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+    <div style="float: left">
+<style>.ig-b- { display: inline-block; }
+.ig-b- img { visibility: hidden; }
+.ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }
+.ig-b-32 { width: 32px; height: 32px; background: url(//badges.instagram.com/static/images/ig-badge-sprite-32.png) no-repeat 0 0; }
+@media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
+.ig-b-32 { background-image: url(//badges.instagram.com/static/images/ig-badge-sprite-32@2x.png); background-size: 60px 178px; } }</style>
+<a href="http://instagram.com/chekkarn?ref=badge" class="ig-b- ig-b-32"><img src="//badges.instagram.com/static/images/ig-badge-32.png" alt="Instagram" /></a>
+      <br />
+      <a href="https://plus.google.com/109227368255007881127?prsrc=3"
+   rel="publisher" target="_top" style="text-decoration:none;">
+<img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/>
+</a>
+    </div>
+    <div class="fb-like" data-href="http://www.facebook.com/linkopingsfallskarmsklubb" data-width="225" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+    
   </div>
 </body>
 </html>
