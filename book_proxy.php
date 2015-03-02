a3bb6f9d7445c99913bd3a71516770f186a520f1
Christian Svensson <blue@cmd.nu>
2015-03-02 19:57:37 +0000

    HTTPS-bypass tunnel proxy for tandem booking

---
+++ book_proxy.php
@@ -0,0 +1,16 @@
+<?php
+// Insecure proxy to show the old booking calendar for tandems
+$url = 'http://www.skydivelfk.com/moduler/tandembokning/Boka1.aspx';
+
+$ch = curl_init($url);
+
+// Random user-agent to make Aspx generate a functioning form *sigh*
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+if (count($_POST) > 0) {
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+}
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+echo str_replace('Boka1.aspx', 'book_proxy.php', curl_exec($ch));
+?>
