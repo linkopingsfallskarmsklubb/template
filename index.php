99ed6c94e6d80d0eb53d59fc1f86bc47d56a3fbd
root <root@joomla-a88b1f46.c.bitnami-builder.internal>
2015-03-11 14:13:55 +0000

    Analytics and background tweak

--- index.php
+++ index.php
@@ -29,7 +29,8 @@ JHtml::_('jquery.ui');
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-60208052-1', 'auto');
+ga('create', 'UA-60538721-1', 'auto');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 
 var menuState = false;
