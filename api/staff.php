74b015b0d90484b3899ce1afed679356cd6b600c
root <root@joomla-a88b1f46.c.bitnami-builder.internal>
2015-07-21 14:27:11 +0000

    Add matrikel functionallity to api

--- api/staff.php
+++ api/staff.php
@@ -2,7 +2,7 @@
 define('_LFK_API', '');
 require_once('api.inc.php');
 
-assert_user_has_view_level('Schedule Admin');
+assert_user_has_view_level('Registered');
 
 header('Content-Type: application/javascript');
 
@@ -21,6 +21,10 @@ if ($type == 'hl') {
   $query = 'SELECT m.InternalNo, FirstName, LastName, m.Club, MAX(mi.Year) as Year FROM skywin.memberinstruct AS mi, skywin.member AS m WHERE m.InternalNo = mi.InternalNo AND InstructType = "T" GROUP BY m.InternalNo';
 } else if ($type == 'foto') {
   $query = 'SELECT InternalNo, FirstName, LastName, Club, Year FROM skywin.member WHERE video="Y"';
+} else if ($type == 'member') {
+  $query = 'SELECT NULLIF(MemberNo, 0) as MemberNo, FirstName, LastName, Address1, Address2, phone1.PhoneNo as PhoneWork, phone2.PhoneNo as PhoneHome, phone3.PhoneNo as PhoneMobile, Emailaddress FROM skywin.member '.
+    'LEFT JOIN skywin.memberphone phone1 ON phone1.InternalNo = member.InternalNo AND phone1.PhoneType = "A" LEFT JOIN skywin.memberphone phone2 ON phone2.InternalNo = member.InternalNo AND phone2.PhoneType = "B" '.
+    'LEFT JOIN skywin.memberphone phone3 ON phone3.InternalNo = member.InternalNo AND phone3.PhoneType = "M" WHERE (pilot="Y" or (club = "LFK" and year > YEAR(NOW()) - 5 and membertype != "PAX")) and LastName != "Övrig"';
 } else {
   exit('[]');
 }
